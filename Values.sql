5adbf12a2b54975cd9880337172356f05b106cf9
Moustafa-F <mfadlall@my.centennialcollege.ca>
2024-11-11 15:48:13 -0500

    Values 1.0
    Includes values for:
    - Tracks
    - Teams

---
+++ Values.sql
@@ -0,0 +1,83 @@
+
+
+--== Tracks ==--
+INSERT INTO TRACKS VALUES(1, 'Daytona International Speedway', 'Daytona Beach, Florida', 4.023);
+INSERT INTO TRACKS VALUES(2, 'Sebring International Raceway', 'Sebring, Florida', 6.019);
+INSERT INTO TRACKS VALUES(3, 'Long Beach Street Circuit', 'Long Beach, California', 3.167);
+INSERT INTO TRACKS VALUES(4, 'WeatherTech Raceway Laguna Seca', 'Monterey, California', 3.602);
+INSERT INTO TRACKS VALUES(5, 'Streets of Detroit', 'Detroit, Michigan', 2.647);
+INSERT INTO TRACKS VALUES(6, 'Watkins Glen International', 'Watkins Glen, New York', 5.552);
+INSERT INTO TRACKS VALUES(7, 'Canadian Tire Motorsport Park', 'Bowmanville, Ontario', 2.459);
+INSERT INTO TRACKS VALUES(8, 'Road America', 'Elkhart Lake, Wisconsin', 6.515);
+INSERT INTO TRACKS VALUES(9, 'Virginia International Raceway', 'Alton, Virginia', 5.262);
+INSERT INTO TRACKS VALUES(10, 'Indianapolis Motor Speedway', 'Speedway, Indiana', 3.925);
+INSERT INTO TRACKS VALUES(11, 'Michelin Raceway Road Atlanta', 'Braselton, Georgia', 4.088);
+
+
+
+
+
+
+--== Teams ==--
+--Grand Touring Prototype (GTP)
+INSERT INTO TEAMS VALUES(7, 'Porsche Penske Motorsport', 'Porsche AG');
+INSERT INTO TEAMS VALUES(1, 'Cadillac Racing', 'General Motors');
+INSERT INTO TEAMS VALUES(31, 'Whelen Cadillac Racing', 'Action Express Racing');
+INSERT INTO TEAMS VALUES(40, 'Wayne Taylor Racing with Andretti', 'Andretti Autosport');
+INSERT INTO TEAMS VALUES(24, 'BMW M Team RLL', 'BMW AG');
+INSERT INTO TEAMS VALUES(5, 'Proton Competition', 'Proton Racing');
+INSERT INTO TEAMS VALUES(85, 'JDC–Miller MotorSports', 'JDC MotorSports');
+INSERT INTO TEAMS VALUES(63, 'Lamborghini – Iron Lynx', 'Iron Lynx');
+
+--Le Mans Prototype 2 (LMP2)
+INSERT INTO TEAMS VALUES(04, 'CrowdStrike Racing by APR', 'CrowdStrike');
+INSERT INTO TEAMS VALUES(2, 'United Autosports USA', 'United Autosports');
+INSERT INTO TEAMS VALUES(8, 'Tower Motorsports', 'DD');
+INSERT INTO TEAMS VALUES(11, 'TDS Racing', 'DD');
+INSERT INTO TEAMS VALUES(18, 'Era Motorsport', 'DD');
+INSERT INTO TEAMS VALUES(20, 'MDK by High Class Racing', 'High Class Racing');
+INSERT INTO TEAMS VALUES(33, 'Sean Creech Motorsport', 'DD');
+INSERT INTO TEAMS VALUES(52, 'Inter Europol by PR1/Mathiasen Motorsports', 'Inter Europol Competition');
+INSERT INTO TEAMS VALUES(74, 'Riley', 'Riley Technologies');
+
+
+--GTD Pro
+INSERT INTO TEAMS VALUES(27, 'Heart of Racing Team', 'Aston Martin');
+INSERT INTO TEAMS VALUES(111, 'Paul Miller Racing', 'BMW AG');
+INSERT INTO TEAMS VALUES(3, 'Corvette Racing by Pratt Miller Motorsports', 'General Motors');
+INSERT INTO TEAMS VALUES(9, 'Pfaff Motorsports', 'McLaren');
+INSERT INTO TEAMS VALUES(14, 'Vasser Sullivan', 'Toyota');
+INSERT INTO TEAMS VALUES(19, 'Iron Lynx', 'Lamborghini');
+INSERT INTO TEAMS VALUES(62, 'Risi Competizione', 'Ferrari');
+INSERT INTO TEAMS VALUES(64, 'Ford Multimatic Motorsports', 'Ford');
+INSERT INTO TEAMS VALUES(75, 'SunEnergy1 Racing', 'Mercedes-Benz');
+INSERT INTO TEAMS VALUES(77, 'AO Racing', 'Porsche');
+INSERT INTO TEAMS VALUES(82, 'DragonSpeed', 'Ferrari');
+
+--GTD
+INSERT INTO TEAMS VALUES(23, 'Triarsi Competizione', 'Ferrari');
+INSERT INTO TEAMS VALUES(13, 'AWA', 'Chevrolet');
+INSERT INTO TEAMS VALUES(21, 'AF Corse', 'Ferrari');
+INSERT INTO TEAMS VALUES(28, 'Flying Lizard Motorsports', 'Aston Martin');
+INSERT INTO TEAMS VALUES(32, 'Korthoff/Preston Motorsports', 'Mercedes-Benz');
+INSERT INTO TEAMS VALUES(34, 'Conquest Racing', 'Ferrari');
+INSERT INTO TEAMS VALUES(43, 'Andretti Motorsports', 'Porsche');
+INSERT INTO TEAMS VALUES(45, 'Magnus Racing', 'Aston Martin');
+INSERT INTO TEAMS VALUES(47, 'Cetilar Racing', 'Ferrari');
+INSERT INTO TEAMS VALUES(57, 'Winward Racing', 'Mercedes-Benz');
+INSERT INTO TEAMS VALUES(66, 'Gradient Racing', 'Acura');
+INSERT INTO TEAMS VALUES(70, 'Inception Racing', 'McLaren');
+INSERT INTO TEAMS VALUES(78, 'Forte Racing', 'Lamborghini');
+INSERT INTO TEAMS VALUES(80, 'Lone Star Racing', 'Mercedes-AMG');
+INSERT INTO TEAMS VALUES(83, 'Iron Dames', 'Lamborghini');
+INSERT INTO TEAMS VALUES(87, 'MDK Motorsports', 'Porsche');
+INSERT INTO TEAMS VALUES(92, 'Kellymoss with Riley', 'Porsche');
+INSERT INTO TEAMS VALUES(96, 'Turner Motorsport', 'BMW');
+INSERT INTO TEAMS VALUES(120, 'Wright Motorsports', 'Porsche');
+
+
+
+
+
+
+
